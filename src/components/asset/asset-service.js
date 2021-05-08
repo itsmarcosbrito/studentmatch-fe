56be1e89ffcbba8857ffac8cd5a71fc7809f8aee
itsmarcosbrito <63377886+itsmarcosbrito@users.noreply.github.com>
2021-05-08 16:56:00 +0100

    localhost updated

--- src/components/asset/asset-service.js
+++ src/components/asset/asset-service.js
@@ -3,25 +3,23 @@ import axios from "axios";
 class AssetService {
   constructor() {
     let service = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: `${process.env.REACT_APP_API_URL}`,
       withCredentials: true,
     });
     this.service = service;
   }
-    getAssets = () => {
-        return this.service.get("/assets")
-            .then((response) => {
-                console.log("assets", response)
-                return response.data
-            });
-    };
-      
+  getAssets = () => {
+    return this.service.get("/assets").then((response) => {
+      console.log("assets", response);
+      return response.data;
+    });
+  };
+
   addAsset = (name) => {
     return this.service
       .post("/assets", { name })
-        .then((response) => response.data);
-      
+      .then((response) => response.data);
   };
-};
+}
 
 export default AssetService;
